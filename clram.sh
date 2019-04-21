6a5fd80369994912dd1de34d9bf085fb883995fa
KhitMinnyo <31130776+KhitMinnyo@users.noreply.github.com>
2019-04-21 13:39:27 +0630

    Update clram.sh

--- clram.sh
+++ clram.sh
@@ -1,25 +1,36 @@
-#!/bin/bash
-#This scripts were created by Khit Minnyo for Kali and Linux systems to clean RAM usage (especially buff/cache memory)
-#for condition
-echo "Before Cleaning"
-#to look up free memory
-free -h
-#clean ram cmds
-sync; echo 1 > /proc/sys/vm/drop_caches
-sync; echo 2 > /proc/sys/vm/drop_caches
-sync; echo 3 > /proc/sys/vm/drop_caches
-#seprate space
+echo '#!/bin/bash' > /usr/bin/clram
+echo '#This scripts were created by Khit Minnyo for Kali and Linux systems to clean RAM usage (especially buff/cache memory)' >> /usr/bin/clram
+echo '#Animation' >> /usr/bin/clram
+echo 'GREEN="\e[92m"' >> /usr/bin/clram
+echo 'BLINK="\e[5m"' >> /usr/bin/clram
+echo 'STOP="\e[0m"' >> /usr/bin/clram
+echo 'printf "${GREEN}"' >> /usr/bin/clram
+echo 'printf "${BLINK}"' >> /usr/bin/clram
+echo '#for condition' >> /usr/bin/clram
+echo 'echo "Before Cleaning"' >> /usr/bin/clram
+echo 'printf "${STOP}"' >> /usr/bin/clram
+echo '#to look up free memory' >> /usr/bin/clram
+echo 'free -h' >> /usr/bin/clram
+echo '#clean ram cmds' >> /usr/bin/clram
+echo 'sync; echo 1 > /proc/sys/vm/drop_caches' >> /usr/bin/clram
+echo 'sync; echo 2 > /proc/sys/vm/drop_caches' >> /usr/bin/clram
+echo 'sync; echo 3 > /proc/sys/vm/drop_caches' >> /usr/bin/clram
+echo '#seprate space' >> /usr/bin/clram
+echo 'echo " "' >> /usr/bin/clram
+echo 'printf "${GREEN}"' >> /usr/bin/clram
+echo 'printf "${BLINK}"' >> /usr/bin/clram
+echo '#to compare ' >> /usr/bin/clram
+echo 'echo "After Cleaning"' >> /usr/bin/clram
+echo 'printf "${STOP}"' >> /usr/bin/clram
+echo '#to check free memory again' >> /usr/bin/clram
+echo 'free -h' >> /usr/bin/clram
+echo '#creator' >> /usr/bin/clram
+echo 'echo "Scripts Created By: "' >> /usr/bin/clram
+echo 'echo "                    Khit Minnyo" ' >> /usr/bin/clram
+echo 'echo "®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®"' >> /usr/bin/clram
+sudo chmod +x /usr/bin/clram
 echo " "
-#to compare 
-echo "After Cleaning"
-#to check free memory again
-free -h
-#creator
-echo "Scripts Created By: "
-echo "          __  ___     _ _    ___  __ _                         "
-echo "          | |/ / |__ (_) |_  |  \/  (_)_ __  _ __  _   _  ___  "
-echo "          | ' /| '_ \| | __| | |\/| | | '_ \| '_ \| | | |/ _ \ "
-echo "          | . \| | | | | |_  | |  | | | | | | | | | |_| | (_) |"
-echo "          |_|\_\_| |_|_|\__| |_|  |_|_|_| |_|_| |_|\__, |\___/ "
-echo "                                                   |___/ "
-echo "®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®"
+echo "Installing Finished"
+echo "Thanks for using CLRAM"
+echo " "
+echo "Please, Visit & Subscribe My Youtube Channel = https://youtube.com/c/KhitMinnyo"
